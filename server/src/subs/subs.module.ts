670b974ffe3b7ff03c6b71694db81c83b6f5ed50
Neezar <rforlyffe@gmaicl.com>
2021-05-09 23:28:51 +0800

    Add subreddit feature

--- server/src/subs/subs.module.ts
+++ server/src/subs/subs.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { CommentsRepository } from 'src/entities/comments/comments.repository';
+import { PostRepository } from 'src/entities/post/post.repository';
+import { SubRepository } from 'src/entities/sub/sub.repository';
 import { SubsController } from './subs.controller';
 import { SubsService } from './subs.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      CommentsRepository,
+      SubRepository,
+      PostRepository,
+    ]),
+    AuthModule,
+  ],
   controllers: [SubsController],
-  providers: [SubsService]
+  providers: [SubsService],
 })
 export class SubsModule {}
