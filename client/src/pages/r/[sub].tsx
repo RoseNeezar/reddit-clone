cfa3391397d7fe9f1579ca20a392583a5cf27036
Neezar <rforlyffe@gmaicl.com>
2021-05-13 17:11:45 +0800

    Add feature to change sub banner

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -1,15 +1,61 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
-import React from "react";
+import { ChangeEvent, createRef, useEffect, useState } from "react";
 import useSWR from "swr";
 import PostCard from "../../components/PostCard";
+import Image from "next/image";
+import classNames from "classnames";
+import { useAuthState } from "../../context/auth";
+import Axios from "axios";
 import { ISub } from "../../typings/types";
 
-const Sub = () => {
+export default function SubPage() {
+  // Local state
+  const [ownSub, setOwnSub] = useState(false);
+  // Global state
+  const { authenticated, user } = useAuthState();
+  // Utils
   const router = useRouter();
-  const { sub: subName }: any = router.query;
-  const { data: sub, error } = useSWR<ISub>(
-    subName ? `/subs/${subName}` : null
-  );
+  const fileInputRef = createRef<HTMLInputElement>();
+
+  const subName = router.query.sub;
+
+  const {
+    data: sub,
+    error,
+    revalidate,
+  } = useSWR<ISub>(subName ? `/subs/${subName}` : null);
+
+  useEffect(() => {
+    if (!sub) return;
+    setOwnSub(authenticated && user?.username === sub.username);
+  }, [sub]);
+
+  const openFileInput = (type: string) => {
+    if (!ownSub) return;
+
+    fileInputRef.current!.name = type;
+    fileInputRef.current!.click();
+  };
+
+  const uploadImage = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files![0];
+
+    const formData = new FormData();
+    formData.append("image", file);
+    formData.append("type", fileInputRef.current!.name);
+
+    try {
+      await Axios.post<ISub>(`/subs/${sub?.name}/image`, formData, {
+        headers: { "Content-Type": "multipart/form-data" },
+      });
+
+      revalidate();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   if (error) router.push("/");
 
   let postsMarkup;
@@ -22,11 +68,76 @@ const Sub = () => {
       <PostCard key={post.identifier} post={post} />
     ));
   }
+
   return (
-    <div className="container flex pt-5">
-      {sub && <div className="w-160">{postsMarkup}</div>}
+    <div>
+      <Head>
+        <title>{sub?.title}</title>
+      </Head>
+
+      {sub && (
+        <>
+          <input
+            type="file"
+            hidden={true}
+            ref={fileInputRef}
+            onChange={uploadImage}
+          />
+          {/* Sub info and images */}
+          <div>
+            {/* Banner image */}
+            <div
+              className={classNames("bg-blue-500", {
+                "cursor-pointer": ownSub,
+              })}
+              onClick={() => openFileInput("banner")}
+            >
+              {sub.bannerUrl ? (
+                <div
+                  className="h-56 bg-blue-500"
+                  style={{
+                    backgroundImage: `url(${sub.bannerUrl})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                  }}
+                ></div>
+              ) : (
+                <div className="h-20 bg-blue-500"></div>
+              )}
+            </div>
+            {/* Sub meta data */}
+            <div className="h-20 bg-white">
+              <div className="container relative flex">
+                <div className="absolute" style={{ top: -15 }}>
+                  <Image
+                    src={sub.imageUrl}
+                    alt="Sub"
+                    className={classNames("rounded-full bg-gray-100", {
+                      "cursor-pointer": ownSub,
+                    })}
+                    onClick={() => openFileInput("image")}
+                    width={70}
+                    height={70}
+                  />
+                </div>
+                <div className="pt-1 pl-24">
+                  <div className="flex items-center">
+                    <h1 className="mb-1 text-3xl font-bold">{sub.title}</h1>
+                  </div>
+                  <p className="text-sm font-bold text-gray-500">
+                    /r/{sub.name}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Posts & Sidebar */}
+          <div className="container flex pt-5">
+            <div className="w-160">{postsMarkup}</div>
+          </div>
+        </>
+      )}
     </div>
   );
-};
-
-export default Sub;
+}
