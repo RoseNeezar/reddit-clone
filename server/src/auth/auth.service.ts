738df4806727863d29acfb01a4b61f2ff31dcbf7
Neezar <rforlyffe@gmaicl.com>
2021-05-15 20:12:58 +0800

    .

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -76,8 +76,6 @@ export class AuthService {
 
   public getCookieForLogOut() {
     return cookie.serialize('token', '', {
-      httpOnly: true,
-
       expires: new Date(0),
       path: '/',
     });
@@ -87,8 +85,6 @@ export class AuthService {
     const payload: TokenPayload = { userId };
     const token = this.jwtService.sign(payload);
     return cookie.serialize('token', token, {
-      httpOnly: true,
-
       maxAge: 604800,
       path: '/',
     });
