53fb81ab957c46bb65990448b220347a26ff8a34
Neezar <rforlyffe@gmaicl.com>
2021-05-16 00:44:14 +0800

    .

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,17 +6,15 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    cors: true,
-  });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   app.set('trust proxy', 1);
-  // app.enableCors({
-  //   credentials: true,
-  //   origin: 'https://leddit-client.vercel.app',
-  //   methods: ['GET', 'PUT', 'POST'],
-  //   allowedHeaders: ['Content-Type', 'Authorization', 'token'],
-  // });
+  app.enableCors({
+    credentials: true,
+    origin: 'https://leddit-client.vercel.app',
+    methods: ['GET', 'PUT', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token', '*'],
+  });
   await app.listen(5000);
 }
 bootstrap();
