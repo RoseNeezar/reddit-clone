83ab2e4547d1c0b427135b1d0a7b892fb9466b2b
Neezar <rforlyffe@gmaicl.com>
2021-05-16 00:18:18 +0800

    trust

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as dotenv from 'dotenv';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
+  app.set('trust proxy', 1);
   app.enableCors({
     credentials: true,
     origin: 'https://leddit-client.vercel.app',
