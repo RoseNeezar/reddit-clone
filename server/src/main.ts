42bf6f2aed6c0bbf5f677b77c260df86fd395186
Neezar <rforlyffe@gmaicl.com>
2021-05-15 17:02:15 +0800

    add dotenv

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors({
