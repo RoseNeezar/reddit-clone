2a78b6d3152e486862729edd2faf549c4914bdd3
Neezar <rforlyffe@gmaicl.com>
2021-05-23 10:53:18 +0800

    fix cookie

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
+  app.set('trust proxy', 1);
   app.enableCors({
     credentials: true,
     origin: process.env.ORIGIN,
     optionsSuccessStatus: 200,
+    methods: ['GET', 'PUT', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token', '*'],
   });
   await app.listen(5000);
 }
